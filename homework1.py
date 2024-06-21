b5fe0d563536f5016d02711fa08d2a425ba7c19f
KA1_SA <swiftkey.vetrov@yandex.ru>
2024-06-21 22:54:44 +0700

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -2,6 +2,6 @@ example = '123456789'
 e = len(str(example)) / 2
 print(example[0])
 print(example[-1])
-print(example[int(e):])#Выведите на экран(в консоль) вторую половину этой строки (С нечётным количеством символов: 'Urban' -> 'ban').
+print(example[int(e):])
 print(example[::-1])
 print(example[1::2])
